135d52049dbc8ecaeb2c70250aba2f2b3acfed9c
Sleep <gastonapares@gmail.com>
2023-10-14 16:24:41 -0300

    comenzando integrador javascript

---
+++ assets/javascript/data.js
@@ -0,0 +1,136 @@
+const productsData = [
+  {
+    id: 1,
+    name: " AMD Ryzen 5600G",
+    bid: 140.0,
+    category: "Procesador",
+    cardImg: "./assets/img/products/5600.jpg",
+  },
+  {
+    id: 2,
+    name: "AMD Ryzen 5600",
+    bid: 140.0,
+    category: "Procesador",
+    cardImg: "./assets/img/products/5600.jpg",
+  },
+  {
+    id: 3,
+    name: "AMD Ryzen 5600X",
+    bid: 140.0,
+    category: "Procesador",
+    cardImg: "./assets/img/products/5600.jpg",
+  },
+  {
+    id: 4,
+    name: "M. Schumacher",
+    bid: 140.0,
+    category: "Placa Madre",
+    cardImg: "./assets/img/products/5600.jpg",
+  },
+  {
+    id: 5,
+    name: "Fernando Alonso",
+    bid: 140.0,
+    category: "Placa Madre",
+    cardImg: "./assets/img/products/5600.jpg",
+  },
+  {
+    id: 6,
+    name: "Dominic Toretto",
+    bid: 140.0,
+    category: "Placa Madre",
+    cardImg: "./assets/img/products/5600.jpg",
+  },
+  {
+    id: 7,
+    name: "Donald Trump",
+    bid: 140.0,
+    category: "Memoria RAM",
+    cardImg: "./assets/img/products/5600.jpg",
+  },
+  {
+    id: 8,
+    name: "Xi Jinping",
+    bid: 140.0,
+    category: "Memoria RAM",
+    cardImg: "./assets/img/products/5600.jpg",
+  },
+  {
+    id: 9,
+    name: "Vladimir Putin",
+    bid: 140.0,
+    category: "Memoria RAM",
+    cardImg: "./assets/img/products/5600.jpg",
+  },
+  {
+    id: 10,
+    name: "Michael Jackson",
+    bid: 140.0,
+    category: "Almacenamiento",
+    cardImg: "./assets/img/products/5600.jpg",
+  },
+  {
+    id: 11,
+    name: "Bruno Mars",
+    bid: 140.0,
+    category: "Almacenamiento",
+    cardImg: "./assets/img/products/5600.jpg",
+  },
+  {
+    id: 12,
+    name: "Bad Bunny",
+    bid: 140.0,
+    category: "Almacenamiento",
+    cardImg: "./assets/img/products/5600.jpg",
+  },
+  {
+    id: 13,
+    name: "Keanu Reeves",
+    bid: 140.0,
+    category: "Notebook",
+    cardImg: "./assets/img/products/5600.jpg",
+  },
+  {
+    id: 14,
+    name: "Jason Stathan",
+    bid: 140.0,
+    category: "Notebook",
+    cardImg: "./assets/img/products/5600.jpg",
+  },
+  {
+    id: 15,
+    name: "Angelina Jolie",
+    bid: 140.0,
+    category: "Notebook",
+    cardImg: "./assets/img/products/5600.jpg",
+  },
+];
+
+// Funcion para dividir el array en X cantidad de arrays
+const DivideProductsInParts = (size) => {
+  let productList = [];
+
+  for (let i = 0; i < productsData.length; i += size) {
+    productList.push(productsData.slice(i, i + size));
+  }
+  return productList;
+};
+
+// Appstate
+const appState = {
+  products: DivideProductsInParts(6),
+  currentProductsIndex: 0,
+  productsLimit: DivideProductsInParts(6).length,
+  activeFilter: null,
+};
+
+// categorias para data
+// Procesador
+// Placa Madre
+// Memoria Memoria RAM
+// Almacenamiento
+// Almacenamiento
+// Teclados
+// Mouse
+// Impresoras
+// Notebooks
---
+++ assets/javascript/index.js
@@ -0,0 +1,251 @@
+// Contenedor de productos
+const productsContainer = document.querySelector(".products-container");
+// Boton ver mas
+const showMoreBtn = document.querySelector(".btn-load");
+// Contenedor Categorias
+const categoriesContainer = document.querySelector(".categories");
+// El HTML Collection de todas las categorias
+const categoriesList = document.querySelectorAll(".category");
+// Carrito
+const cartBtn = document.querySelector(".cart-label");
+// Boton para abrir y cerrar el menu
+const menuBtn = document.querySelector(".menu-label");
+// Carrito div
+const cartMenu = document.querySelector(".cart");
+// Menu (Hamburguesa)
+const barsMenu = document.querySelector(".navbar-list");
+// Overlay
+const overlay = document.querySelector(".overlay");
+// Cart bubble
+const cartBubble = document.querySelector(".cart-bubble");
+// Total del carrito
+const total = document.querySelector(".total");
+// Boton de comprar
+const buyBtn = document.querySelector(".btn-buy");
+// Boton para borrar
+const deleteBtn = document.querySelector(".btn-delete");
+// Cart container
+const productsCart = document.querySelector(".cart-container");
+
+// Seteamos el carrito
+let cart = [];
+
+/* --------- Logica reder products ---------- */
+// Funcion para crear el html del producto
+// TODO: AGREGAR DATASETS AL BUTTON
+const createProductTemplate = (product) => {
+  const { id, name, bid, cardImg, category } = product;
+  return `
+  <div class="discovery--product">
+    <img
+      src="${cardImg}"
+      alt="${name}"
+      class="discovery--product--img"
+    />
+
+    <h3 class="discovery--product--name">${category} ${name}</h3>
+    <div class="discovery--product--buy">
+      <button class="buy--btn" data-id='${id}'
+               data-name='${name}'
+              data-bid='${bid}'
+              data-img='${cardImg}'>Agregar al carrito</button>
+      <p class="buy--price">${bid}</p>
+    </div>
+  </div>;
+  `;
+};
+
+// Funcion para renderizar productos
+const renderProducts = (productList) => {
+  productsContainer.innerHTML += productList
+    .map(createProductTemplate)
+    .join("");
+};
+
+/* --------- Logica ver mas ---------- */
+
+// Funcion para saber si estamos al final del array
+const isLastIndexOf = () => {
+  return appState.currentProductsIndex === appState.productsLimit - 1;
+};
+
+// Funcion para renderizar mas productos cuando apretamos ver mas
+const showMoreProducts = () => {
+  appState.currentProductsIndex += 1;
+
+  let { products, currentProductsIndex } = appState;
+
+  renderProducts(products[currentProductsIndex]);
+
+  if (isLastIndexOf()) {
+    showMoreBtn.classList.add("hidden");
+  }
+};
+
+// Funcion para ocultar el boton ver mas si hay una categoria seleccionada
+const setShowMoreVisibility = () => {
+  if (!appState.activeFilter) {
+    showMoreBtn.classList.remove("hidden");
+  }
+
+  showMoreBtn.classList.add("hidden");
+};
+
+/* --------- Logica filtros ---------- */
+// Funcion para cambiar el estado de los botones de las categorias
+const changeBtnActiveState = (selectedCategory) => {
+  const categories = [...categoriesList];
+
+  categories.forEach((categoryBtn) => {
+    if (categoryBtn.dataset.category !== selectedCategory) {
+      categoryBtn.classList.remove("active");
+      return;
+    }
+
+    categoryBtn.classList.add("active");
+  });
+};
+
+// Funcion para cambiar el estado del filtro activo
+const changeFiltersState = (btn) => {
+  appState.activeFilter = btn.dataset.category;
+  changeBtnActiveState(appState.activeFilter);
+  setShowMoreVisibility(appState.activeFilter);
+};
+
+// Funcion para filtrar los productos
+const renderFilteredProducts = () => {
+  const filteredProducts = productsData.filter(
+    (product) => product.category === appState.activeFilter
+  );
+
+  renderProducts(filteredProducts);
+};
+
+// Funcion para aplicar filtro
+const applyFilter = ({ target }) => {
+  if (!isInactiveFilterBtn(target)) return;
+  changeFiltersState(target);
+  productsContainer.innerHTML = "";
+  if (appState.activeFilter) {
+    renderFilteredProducts();
+    appState.currentProductsIndex = 0;
+    return;
+  }
+  renderProducts(appState.products[0]);
+};
+
+// Funcion para saber si el elemento que apretamos es un boton de categoria y no esta activo
+const isInactiveFilterBtn = (element) => {
+  return (
+    element.classList.contains("category") &&
+    !element.classList.contains("active")
+  );
+};
+
+/* --------- Logica Menu/Carrito Modal ---------- */
+const toggleCart = () => {
+  cartMenu.classList.toggle("open-cart");
+
+  if (barsMenu.classList.contains("open-menu")) {
+    barsMenu.classList.remove("open-menu");
+    return;
+  }
+
+  overlay.classList.toggle("show-overlay");
+};
+
+const toggleMenu = () => {
+  barsMenu.classList.toggle("open-menu");
+
+  if (cartMenu.classList.contains("open-cart")) {
+    cartMenu.classList.remove("open-cart");
+    return;
+  }
+  overlay.classList.toggle("show-overlay");
+};
+
+// Funcion para cerrar el menu cuando se clickea el overlay
+const closeOnOverlayClick = () => {
+  barsMenu.classList.remove("open-menu");
+  cartMenu.classList.remove("open-cart");
+  overlay.classList.remove("show-overlay");
+};
+
+// Funcion para cerrar menues, cuando scrolleamos
+const closeOnScroll = () => {
+  if (
+    barsMenu.classList.contains("open-menu") &&
+    cartMenu.classList.contains("open-cart")
+  ) {
+    return;
+  }
+
+  barsMenu.classList.remove("open-menu");
+  cartMenu.classList.remove("open-cart");
+  overlay.classList.remove("show-overlay");
+};
+
+/* --------- --------------- ---------- */
+/* --------- Logica carrito  ---------- */
+/* --------- --------------- ---------- */
+// Render carrito
+const renderCart = () => {
+  if (!cart.length) {
+    productsCart.innerHTML = `<p class="empty-msg">Agrega un producto raton</p>`;
+    return;
+  }
+
+  alert("tuki");
+};
+
+const addProduct = (e) => {
+  if (!e.target.classList.contains("btn-add")) return;
+  const product = e.target.dataset;
+
+  // Tendriamos que hacer un if para verificar que el producto a agregar no este en el carrito
+  if (isExistingCartProduct(product)) {
+    addUnitToProduct(product);
+  } else {
+    createCartProduct(product);
+  }
+
+  renderCart();
+  console.log(cart);
+};
+
+// Funcion para agregar una unidad al producto
+const addUnitToProduct = (product) => {
+  cart = cart.map((cartProduct) =>
+    cartProduct.id === product.id
+      ? { ...cartProduct, quantity: cartProduct.quantity + 1 }
+      : cartProduct
+  );
+};
+
+// Funcion para saber si un producto ya existe en el carrito
+const isExistingCartProduct = (product) => {
+  return cart.find((item) => item.id === product.id);
+};
+
+// Funcion para crear un objeto con la info del produ que queremos agregar al carrito
+const createCartProduct = (product) => {
+  cart = [...cart, { ...product, quantity: 1 }];
+};
+
+// Funcion init
+const init = () => {
+  renderProducts(appState.products[0]);
+  showMoreBtn.addEventListener("click", showMoreProducts);
+  categoriesContainer.addEventListener("click", applyFilter);
+
+  cartBtn.addEventListener("click", toggleCart);
+  menuBtn.addEventListener("click", toggleMenu);
+  overlay.addEventListener("click", closeOnOverlayClick);
+  window.addEventListener("scroll", closeOnScroll);
+
+  productsContainer.addEventListener("click", addProduct);
+  document.addEventListener("DOMContentLoaded", renderCart);
+};
+
+init();
